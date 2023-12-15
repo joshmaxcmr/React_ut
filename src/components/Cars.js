f3fce002306ad96632edd8e12c046aa705aaceb7
johann maxime <johannmaxime@yahoo.fr>
2023-12-15 04:40:21 +0100

    chap 3 004 State

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Car = ({ children, color }) => {
+const Car = ({ children, color,year }) => {
 
     const colorInfo = color ? (<p>Couleur: {color} </p>) : (<p>Néant</p>);
 
@@ -8,6 +8,7 @@ const Car = ({ children, color }) => {
         return (
             <div style={{ backgroundColor: 'pink', width: '400px', padding: '10px', margin: '5px auto' }}>
                 <p>Marque: {children} </p>
+                <p>Age: {year} </p>
                 {colorInfo}
             </div>
         )
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import Car from "./Cars";
 
 class Mycars extends Component {
+
+   state ={
+      voitures:[
+         {name: 'Ford', color:'red',year:'2000'},
+         {name: 'Mercedes', color:'Black',year:'2022'},
+         {name: 'Porche', color:'yellow',year:'2018'},
+      ]
+   }
+
    render() {
       return (
          <div>
             <h1>{this.props.title}</h1>
 
-            <Car color="red">Mercedes</Car>
-            <Car >Ferrari</Car>
-            <Car color="green"></Car>
+            <Car color={this.state.voitures[0].color} year={this.state.voitures[0].year}>{this.state.voitures[0].name}</Car>
+            <Car color={this.state.voitures[1].color} year={this.state.voitures[1].year}>{this.state.voitures[1].name}</Car>
+            <Car color={this.state.voitures[2].color
+            } year={this.state.voitures[2].year}>{this.state.voitures[2].name}</Car>
          </div>
       )
    }
